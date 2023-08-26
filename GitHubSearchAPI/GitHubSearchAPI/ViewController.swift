124ff23093d80d2796c982330e7cd543da6dc8e5
보니 <73548875+Guboneui@users.noreply.github.com>
2023-08-27 02:17:13 +0900

    [FEAT] Make Project with Initial Setting

---
+++ GitHubSearchAPI/GitHubSearchAPI/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  GitHubSearchAPI
+//
+//  Created by 구본의 on 2023/08/27.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    setupViews()
+  }
+  
+  private func setupViews() {
+    title = "SearchMain"
+    view.backgroundColor = .white
+  }
+}
